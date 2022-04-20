09bde4ada37dba889031b449ff99b0c339027224
MaciejZdanski <zdanski.maciej@gmail.com>
2022-04-20 19:18:37 +0200

    a

--- node/index.js
+++ node/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// KONFIGURACJA STATYCZNEGO FOLDERU
+app.use(express.static(path.join(__dirname, '/src/static')))
+
+// POBIERANIE INFORMACJI OD KLIENTA
+app.post('/sendForm', (req, res) => {
+  console.log(req.query)
+  res.send("ok")
+})
+
+// WYSYŁANIE KOMENTARZY Z SERWERA DO KLIENTA
+app.get('/getComments', (req, res) => {
+  
+  res.send("ok")
 })
 
 app.listen(80)
